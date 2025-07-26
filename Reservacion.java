9bf8dad80e7c3f50b4041a31510eb9cc12e646f3
jeffjimt <jimenezjefferson70@gmail.com>
2025-07-25 19:21:31 -0600

    Add files via upload
    Commit del sistema de reservacion

---
+++ Reservacion.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectohotelg5;
+
+/**
+ *
+ * @author bayro
+ */
+public class Reservacion {
+    
+    private String clienteCedula;
+    private int numeroHabitacion;
+    private String fechaEntrada;
+    private String fechaSalida;
+
+    public Reservacion() {
+    }
+
+    public Reservacion(String clienteCedula, int numeroHabitacion, String fechaEntrada, String fechaSalida) {
+        this.clienteCedula = clienteCedula;
+        this.numeroHabitacion = numeroHabitacion;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+    }
+
+    public String getClienteCedula() {
+        return clienteCedula;
+    }
+
+    public void setClienteCedula(String clienteCedula) {
+        this.clienteCedula = clienteCedula;
+    }
+
+    public int getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(int numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+
+    public String getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(String fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public String getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(String fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+}
