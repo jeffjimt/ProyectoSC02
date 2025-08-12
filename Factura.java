519da677073acc38d3c8efe1d009749e2c04564b
jeffjimt <jimenezjefferson70@gmail.com>
2025-08-12 09:36:54 -0600

    Commit factura terminado
    Se hace commit con la clase factura con las funciones optimas para el proyecto ya terminado

--- Factura.java
+++ Factura.java
@@ -46,4 +46,54 @@ public class Factura {
     public void setDetalle(String detalle) {
         this.detalle = detalle;
     }
+    
+    
+    
+  public void generarFactura(Reservacion[] reservaciones, Servicio[] servicios, int reservacionesCount, int serviciosCount,
+                          String[] serviciosPorCliente, int serviciosPorClienteCount) {
+    double total = 0;
+    StringBuilder sbDetalle = new StringBuilder();
+
+    // Sumar reservaciones 
+    for (int i = 0; i < reservacionesCount; i++) {
+        if (reservaciones[i].getClienteCedula().equals(this.clienteCedula)) {
+            Reservacion r = reservaciones[i];
+            double costoHabitacion = 25000;  
+            total += costoHabitacion;
+            sbDetalle.append("Habitación No. ").append(r.getNumeroHabitacion())
+                .append(" desde ").append(r.getFechaEntrada())
+                .append(" hasta ").append(r.getFechaSalida())
+                .append(" - ₡").append(costoHabitacion).append("\n");
+        }
+    }
+
+    // sumar servicios solicitados por el cliente
+    boolean encontroServicio;
+    for (int i = 0; i < serviciosCount; i++) {
+        Servicio s = servicios[i];
+        encontroServicio = false;
+        for (int j = 0; j < serviciosPorClienteCount; j++) {
+            String sc = serviciosPorCliente[j];
+            if (sc.equals(this.clienteCedula + "-" + s.getNombre())) {
+                encontroServicio = true;
+                break;
+            }
+        }
+        if (encontroServicio) {
+            total += s.getPrecio();
+            sbDetalle.append("Servicio: ").append(s.getNombre())
+                .append(" - ₡").append(s.getPrecio()).append("\n");
+        }
+    }
+
+    this.montoTotal = total;
+    this.detalle = sbDetalle.toString();
+}
+
+    // Método para mostrar la factura
+    public String mostrarFactura() {
+        return "Factura para cliente cédula: " + clienteCedula + "\n" +
+               "Detalle:\n" + detalle + "\n" +
+               "Monto total: ₡" + montoTotal;
+    }
 }
